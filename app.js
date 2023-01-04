a8e821188eb58c26d2e70becb04eb7c02a605181
Saurabh <saurabhnegi0007@gmail.com>
2023-01-04 10:27:53 +0530

    login route created

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.send("<h1>Hello Auth System</h1>")
 })
 
-app.post('/register', async(req, res) => {
+app.post('/register', async (req, res) => {
     try {
         //collect all info
         const { firstname, lastname, email, password } = req.body;
@@ -56,10 +56,50 @@ app.post('/register', async(req, res) => {
     } catch(error) {
         console.log(error);
         console.log("connection failed");
+    } 
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        //collect info
+        const {email, password} = req.body;
+
+        //validate if all info present
+        if(!(email && password)) {
+            res.status(401).send("Enter all details");
+        }
+
+        //check if user exists
+        const user = User.findOne({email});
+        if(user && (await bcrypt.compare(password, user.password))) {
+            //create a toke
+            const token = jwt.sign({
+                id: user._id, email
+            },
+                'shhhh',
+                {expiresIn:'2h'}
+            )
+
+            user.token = token;
+            user.password = undefined;
+
+            //cookies
+            const options = {
+                expires: new Date(Date.now(3 * 24 * 60 * 60 * 1000)),
+                httpsOnly: true
+            }
+
+            res.status(200).cookie("token", token, options).json({
+                success: true,
+                token,
+                user
+            })
+        }
+        res.sendStatus(400).send("email or password is incorrect")
+    } catch (error) {
+        console.log(error);
+        console.log("login failed");
     }
-
-
-    
 })
 
 app.listen(port, () => {
